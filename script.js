ce3b99ff81a8874c76a81e699a1f3c95dc8305e0
Sarthak12397 <ssarthak737@gmail.com>
2024-06-02 08:51:44 +0545

    Slider

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let items = document.querySelectorAll('.slider .list .item');
+    let thumbnails = document.querySelectorAll('.thumbnail .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    let itemActive = 0;
+
+    next.addEventListener('click', function() {
+        showSlider((itemActive + 1) % items.length);
+    });
+
+    prev.addEventListener('click', function() {
+        showSlider((itemActive - 1 + items.length) % items.length);
+    });
+
+    thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', function() {
+            showSlider(index);
+        });
+    });
+
+    function showSlider(index) {
+        items[itemActive].classList.remove('active');
+        thumbnails[itemActive].classList.remove('active');
+        items[index].classList.add('active');
+        thumbnails[index].classList.add('active');
+        itemActive = index;
+    }
+});
+
 window.addEventListener('scroll', function(){
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
